37333aa540d43d4f46c71244d5b677897329972d
NUNIYA <nuniyat.g@gmail.com>
2025-03-18 18:45:24 +0300

    fc

---
+++ firstwebsite/resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<head>
+    <title>laravel</title>
+</head>
+
+<body>
+    <h1> contact page</h1>
+</body>
+
+</html>
---
+++ firstwebsite/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<head>
+    <title>laravel</title>
+</head>
+
+<body>
+    <h1>New home page</h1>
+</body>
+
+</html>
